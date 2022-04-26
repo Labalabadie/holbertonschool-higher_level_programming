ea260f728b09566dfce8e513f69ee313517c4d62
Agustin Labadie <4433@holbertonschool.com>
2022-04-26 11:26:00 -0700

    Task 1, ayudita de nacho

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
+from math import fmod
 number = random.randint(-10000, 10000)
-if number < 0:
-    numero = -number
-else:
-    numero = number
 
-if (numero % 10) > 5:
-    print("Last digit of", number, "is", (numero % 10), "and is greater than 5")
-elif (numero % 10) == 0:
+if fmod(number, 10) > 5:
+    print("Last digit of", number, "is", int(fmod(number, 10)), end='')
+    print(" and is greater than 5")
+elif fmod(number, 10) == 0:
     print("Last digit of", number, "is 0 and is 0")
-elif (numero % 10) < 6:
-    print("Last digit of", number, "is", numero % 10, "and is less than 6")
+elif fmod(number, 10) < 6:
+    print("Last digit of", number, "is", int(fmod(number, 10)), end='')
+    print(" and is less than 6 and not 0")
