a9010d5df253e00525170f90a4f594ddfcd9bdf7
Agustin Labadie <4433@holbertonschool.com>
2022-04-26 12:44:42 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 a = 0
 b = 1
-for a in range(0, 9):
+for a in range(0, 8):
     b = a + 1
     for b in range(int(b), 10):
-        print(a, b,sep='', end=', ')
+        print(a, b, sep='', end=', ')
 print("89")
