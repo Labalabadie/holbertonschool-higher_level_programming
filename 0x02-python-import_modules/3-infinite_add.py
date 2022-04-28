6357be5c2e83f3cc72b3505f52c8090b08dcfb3d
Agustin Labadie <4433@holbertonschool.com>
2022-04-28 13:28:04 -0700

    mas prolijo la cosa

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     from sys import argv
     from add_0 import add
+    a = 0
     if len(argv) != 1:
-        a = int(argv[1])
         for i in range(len(argv) - 1):
             a = add(a, int(argv[i + 1]))
-        print(a - int(argv[1]))
+        print(a)
     else:
         print("0")
