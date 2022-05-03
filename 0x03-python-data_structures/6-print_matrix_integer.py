e3eaa9e0b1858ff16b1f10b9d21e78275ff4849a
Agustin Labadie <4433@holbertonschool.com>
2022-05-03 12:56:05 -0700

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
     for line in matrix:
-        print ('  '.join(map(str, line)))
+        print('  '.join(map(str, line)))
