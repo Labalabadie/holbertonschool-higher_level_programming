7ddea86f89ea4afa9d65f4d2b4bc97a1f4422510
Agustin Labadie <4433@holbertonschool.com>
2022-05-03 11:56:44 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     s = ""
     for i in range(len(my_string)):
         if my_string[i] != 'C' and my_string[i] != 'c':
-            s +=  my_string[i]
+            s += my_string[i]
     return s
