542cfd920ff5071db1373b83da39c9e021a75727
Agustin Labadie <4433@holbertonschool.com>
2022-05-04 12:50:34 -0700

    task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,8 +3,8 @@ def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
     for i, line in enumerate(matrix):
         new_matrix[i] = list(map(pow, line))
-        
     return new_matrix
+
 
 def pow(x):
     return x * x
