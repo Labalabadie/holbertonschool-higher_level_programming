2f425f7dc1dd4071a94e3a3c75fbc2cbabab88a1
Agustin Labadie <4433@holbertonschool.com>
2022-05-04 12:48:35 -0700

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+    for i, line in enumerate(matrix):
+        new_matrix[i] = list(map(pow, line))
+        
+    return new_matrix
+
+def pow(x):
+    return x * x
