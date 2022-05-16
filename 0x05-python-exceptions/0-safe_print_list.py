be1fabe7a27abadbee32dd136df1608d053990a1
Agustin Labadie <4433@holbertonschool.com>
2022-05-16 15:17:06 -0700

    pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     n = 0
     for i in range(x):
-        try:    
-            print("{:d}".format(my_list[i]), end ="")
+        try:
+            print("{:d}".format(my_list[i]), end="")
             n += 1
-        except:
+        except (IndexError, ValueError, TypeError):
             pass
     print()
     return n
