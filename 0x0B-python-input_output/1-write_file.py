cfae37478a625b2b952a21e84d8c3edd1f160e55
Agustin Labadie <4433@holbertonschool.com>
2022-05-31 13:52:44 -0700

    trying comments

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that writes a string to a text file"""
 def write_file(filename="", text=""):
+    """Opens file in writing mode"""
     with open(filename, mode='w', encoding='utf-8') as b_file:
         return (b_file.write(text))
