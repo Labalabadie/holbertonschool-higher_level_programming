9f417d59d09aa3274ea8e02fb9f3dfe007a8b5f3
Agustin Labadie <4433@holbertonschool.com>
2022-05-31 14:37:05 -0700

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that creates object from a JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Creates Objet from a "JSON" file"""
+    with open(filename) as e_file:
+        return (json.dumps(e_file.read()))
