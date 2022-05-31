f2f723850e885685a766405ae71e3ad5da483e50
Agustin Labadie <4433@holbertonschool.com>
2022-05-31 13:48:04 -0700

    new comments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module that read from a file, printing its content"""
 def read_file(filename=""):
+    """opens file"""
     with open(filename, encoding='utf-8') as a_file:
+        """iterates printing each line"""
         for a_line in a_file:
             print(a_line, end="")
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,4 +5,5 @@ import json
 
 
 def to_json_string(my_obj):
+    """cast an object as a string and returns it"""
     return json.dumps(my_obj)
