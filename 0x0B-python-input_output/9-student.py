2b7cbe5f24e2edd81a86d8689ee7cc85263c848d
Agustin Labadie <4433@holbertonschool.com>
2022-05-31 19:55:09 -0700

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that creates a class student """
+
+
+class Student:
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """ New instance"""
+        self.first_name = first_name
+        self.last_nmae = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a JSON of the class"""
+        return self.__dict__
