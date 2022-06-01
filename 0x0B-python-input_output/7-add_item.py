9da1479039922b5bc77c125479bf22b115c7ed4a
Agustin Labadie <4433@holbertonschool.com>
2022-06-01 08:11:21 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,13 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-my_list = []
+
 try:
     my_list = load_from_json_file("add_item.json")
 except Exception:
+    my_list = [(sys.argv[1:])]
     save_to_json_file(sys.argv[1:], "add_item.json")
+    exit()
 
-my_list.append(sys.argv[1:])
+my_list += (sys.argv[1:])
 save_to_json_file(my_list, "add_item.json")
