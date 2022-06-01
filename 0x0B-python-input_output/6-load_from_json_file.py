ea9ceae780cd96538186513d328993f159400c00
Agustin Labadie <4433@holbertonschool.com>
2022-06-01 07:55:47 -0700

    Task 7

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 
 def load_from_json_file(filename):
     """Creates Objet from a "JSON" file"""
-    with open(filename) as e_file:
+    with open(filename,'r', encoding="utf-8") as e_file:
         return (json.loads(e_file.read()))
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script, adds all args to a py list, then saves to file"""
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+import sys
+
+
+try:
+    my_list = load_from_json_file("add_item.json")
+except Exception:
+    save_to_json_file(sys.argv[1:] , "add_item.json")
+
+my_list.append(sys.argv[1:])
+save_to_json_file(my_list , "add_item.json")
