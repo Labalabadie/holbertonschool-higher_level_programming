20c7ddea5fd1db800acca8f8977f917cbe30ab46
Agustin Labadie <4433@holbertonschool.com>
2022-06-01 05:51:14 -0700

    grammar

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Module that creates a class student """
 
+
 class Student:
     """Class Student"""
 
     def __init__(self, first_name, last_name, age):
         """ New instance"""
         self.first_name = first_name
-        self.last_nmae = last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
