1bfe753ee5600113a5d8428f1eb391daa3f64400
Agustin Labadie <4433@holbertonschool.com>
2022-05-31 19:59:28 -0700

    b

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,13 +5,11 @@
 class Student:
     """Class Student"""
 
-
     def __init__(self, first_name, last_name, age):
         """ New instance"""
         self.first_name = first_name
         self.last_nmae = last_name
         self.age = age
-
 
     def to_json(self):
         """Returns a JSON of the class"""
