10a6db3199351b97fc39f305ab8b9b690f8ff8de
Agustin Labadie <labalabadie@gmail.com>
2022-06-02 13:12:16 -0700

    trancado, voy a l gym

--- 0x0C-python-almost_a_circle/models/rectangle_safe.py
+++ 0x0C-python-almost_a_circle/models/rectangle_safe.py
@@ -14,13 +14,6 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
-    def type_int_validator(var, name):
-        """Hola pie te quedaste aca, tenes que validar todos los casos ejercicio 3 sin escribir todo a mano, besitos bestios chau chau, """
-        if type(var) is not int
-            raise TypeError("{name} must be an interger",)
-        if width <= 0:
-            raise ValueError("width must be > 0")
-        
     @property
     def width(self):
         """Width getter"""
@@ -28,23 +21,15 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """Width setter"""
-        if type(width) is not int:
-            raise TypeError("width must be an interger")
-        if width <= 0:
-            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
-    def height(self):
+    def height(elf):
         """height getter"""
         return (self.__height)
     @height.setter
     def height(self, height):
         """height setter"""
-        if type(height) != type(int):
-            raise TypeError("height must be an interger")
-        if height <= 0:
-            raise ValueError("height must be > 0")
         self.__height = height
             
     @property
@@ -54,10 +39,6 @@ class Rectangle(Base):
     @width.setter
     def x(self, x):
         """x setter"""
-        if type(x) is not int:
-            raise TypeError("x must be an interger")
-        if x < 0:
-            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -67,8 +48,4 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """y setter"""
-        if type(y) is not int:
-            raise TypeError("y must be an interger")
-        if y < 0:
-            raise ValueError("y must be >= 0")
         self.__y = y
