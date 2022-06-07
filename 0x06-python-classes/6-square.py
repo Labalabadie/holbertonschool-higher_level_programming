30ff29e3b6e750acf514ea71416181bd8236211f
Agustin Labadie <4433@holbertonschool.com>
2022-06-07 10:40:11 -0700

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,6 +20,8 @@ class Square:
 
     def my_print(self):
         """Prints in stdout, square of '#'"""
+        for n in range(self.__position[1]):
+            print()
         for i in range(self.size):
             for k in range(self.__position[0]):
                 print(" ", end="")
