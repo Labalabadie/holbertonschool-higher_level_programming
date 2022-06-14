8d0941ba32bd1d92ac2c3283fdd301aa31743a88
Agustin Labadie <4433@holbertonschool.com>
2022-06-14 12:26:58 -0700

    raise error in init

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,6 +9,15 @@ class Rectangle:
         """Initializes instance"""
         self.height = height
         self.width = width
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if type(height) is not int:
+            raise TypeError("width must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
 
     def area(self):
         """returns area"""
