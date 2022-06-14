ab760fc2e39a7fcb6987e1ae99da979fe7eff2a4
Agustin Labadie <4433@holbertonschool.com>
2022-06-14 12:18:34 -0700

    Dia de la marmota

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes instance"""
+        self.height = height
+        self.width = width
+
+    def area(self):
+        """returns area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Returns perimeter"""
+        return (self.__width + self.__height) + 2
+
+    @property
+    def width(self):
+        """Width getter"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Height setter"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Width getter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Height getter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
