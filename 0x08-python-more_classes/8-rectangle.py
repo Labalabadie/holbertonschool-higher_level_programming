7204a317059c353d354e98da084768a18ab6cec3
Agustin Labadie <4433@holbertonschool.com>
2022-06-20 12:01:42 -0700

    task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -39,6 +39,17 @@ class Rectangle:
         """Returns perimeter"""
         return self.__width*2 + self.__height*2
 
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns biggest rectangle based on area"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+
     @property
     def width(self):
         """Width getter"""
