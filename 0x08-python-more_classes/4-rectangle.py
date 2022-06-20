b9a033fb9d5d08ccb51193c50dbbbbb7620ec6e2
Agustin Labadie <4433@holbertonschool.com>
2022-06-20 11:34:42 -0700

    task 4

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes instance"""
+        self.height = height
+        self.width = width
+
+    def __str__(self):
+        """Returns a str representation of a rectangle instance with '#'"""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        s_str = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                s_str += '#'
+            s_str += '\n'
+        return s_str[:-1]
+
+    def __repr__(self):
+        """Representation of a rectangle"""
+        return (f"{self.__class__.__name__}({self.width}, {self.height})")
+
+    def area(self):
+        """returns area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Returns perimeter"""
+        return self.__width*2 + self.__height*2
+
+    @property
+    def width(self):
+        """Width getter"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Height setter"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Width getter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Height getter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
