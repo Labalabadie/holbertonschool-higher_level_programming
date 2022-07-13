c2f7f30a9fd9ca1523ee5f343bec168e7be37f0f
Agustin Labadie <4433@holbertonstudents.com>
2022-07-13 14:14:12 -0700

    15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- THIS is comment
+SELECT tv_shows.title AS title FROM tv_show_genres
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "COMEDY"
+ORDER BY title;
