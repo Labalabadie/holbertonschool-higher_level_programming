469300156a2444a46079bd452111f20a5adc7073
Agustin Labadie <labalabadie@gmail.com>
2022-08-01 15:57:48 -0700

    new ap\proach

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-let kakaroto = 'C is fun';
-console.log(kakaroto);
-kakaroto = 'Python is cool';
-console.log(kakaroto);
-kakaroto = 'JavaScript is amazing';
+let kakaroto = 'C is fun\nPython is cool\nJavascript is amazing';
 console.log(kakaroto);
