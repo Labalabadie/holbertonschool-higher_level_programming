2a1c1c06782c4533bea2ca739a65766acbbd16ee
Agustin Labadie <labalabadie@gmail.com>
2022-08-01 15:55:30 -0700

    tak1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let kakaroto = 'C is fun';
+console.log(kakaroto);
+kakaroto = 'Python is cool';
+console.log(kakaroto);
+kakaroto = 'JavaScript is amazing';
+console.log(kakaroto);
