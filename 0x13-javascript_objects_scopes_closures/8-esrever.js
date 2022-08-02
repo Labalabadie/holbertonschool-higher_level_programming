48dcc3c85342a7e24c9b3a5c79758ba14cb8fb5b
Agustin Labadie <labalabadie@gmail.com>
2022-08-02 15:04:37 -0700

    8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const NewList = [];
   for (let i = 0; i < list.lenght; i++) {
-    NewList.push(list[i]);
+    NewList.unshift(list[i]);
   }
   return (NewList);
 };
