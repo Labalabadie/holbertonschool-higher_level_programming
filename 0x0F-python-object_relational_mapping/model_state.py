425cba88d349b5ef6670fc2d6dab40b18035b0a4
labalabadie <labalabadie@gmail.com>
2022-08-07 08:53:51 -0700

    base state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """ states module """
-""" contains the class definition of a State and an instance Base = declarative_base() """
-""" WARNING: all classes who inherit from Base must be imported before calling Base.metadata.create_all(engine) """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String # ??
-from sys import argv
-
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class State(Base):
-	"""Class State"""
-	__tablename__ = 'states'
-	id = Column(Integer, primary_key=True, unique=True)
-	name = Column(String(128), nullable=False)
+    """Class State"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(128), nullable=False)
