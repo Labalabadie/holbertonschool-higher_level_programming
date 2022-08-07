b738a96fbb805ad910b0f282831568723e257f4e
labalabadie <labalabadie@gmail.com>
2022-08-07 07:49:34 -0700

    5 with string method

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,14 +7,10 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities, states WHERE cities.state_id = states.id AND states.name=%(arg_state)s\
+    cur.execute("SELECT cities.name FROM cities, states \
+            WHERE cities.state_id = states.id AND states.name=%(arg_state)s\
             ORDER BY states.id ASC", {'arg_state': argv[4]})
     states = cur.fetchall()
-    for i in range(len(states)):
-        print(str(states[i][0]), end="")
-        if i != (len(states) - 1):
-            print(", ", end="")
-        else:
-            print()
-        cur.close()
+    print(", ".join([state[0] for state in states]))
+    cur.close()
     db.close()
