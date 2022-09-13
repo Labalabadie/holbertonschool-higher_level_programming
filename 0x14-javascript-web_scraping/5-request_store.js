9cd19e1eae7b733aa09e5214ae31920ac908b0bd
Agustin Labadie <labalabadie@gmail.com>
2022-09-13 09:48:52 -0700

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const axios = require('axios').default;
+const fs = require('fs');
+
+const url = 'http://loripsum.net/api loripsum';
+
+axios.get(url)
+  .then(function (response) {
+    // handle success
+
+    fs.writeFile(process.argv[3], response.data, 'utf-8', function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
